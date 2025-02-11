2b4bf8a61159a448727f764a931ed741f53466af
RonanKer29 <kervella.ronan@hotmail.com>
2025-02-11 11:37:17 +0100

    project page

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,12 @@
 import Button from "./Button";
+import SelectedProject from "./SelectedProject";
 
-const ProjectsSidebar = ({ onStartAddProject, projects }) => {
+const ProjectsSidebar = ({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  SelectedProjectId,
+}) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -11,13 +17,24 @@ const ProjectsSidebar = ({ onStartAddProject, projects }) => {
       </div>
 
       <ul className="mt-8">
-        {projects.map((projects) => (
-          <li key={projects.id}>
-            <button className="w-full px-2 py-1 my-1 text-left rounded-sm text-stone-400 hover:text-stone-200 hover:bg-stone-800">
-              {projects.title}
-            </button>
-          </li>
-        ))}
+        {projects.map((projects) => {
+          let cssClasses =
+            "w-full px-2 py-1 my-1 text-left rounded-sm  hover:text-stone-200 hover:bg-stone-800";
+
+          if (project.id === SelectedProjectId) {
+            cssClasses += "bg-stone-800 text-stone-200";
+          } else {
+            cssClasses += "text-stone-400";
+          }
+
+          return (
+            <li key={projects.id}>
+              <button className={cssClasses} onClick={onSelectProject}>
+                {projects.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
